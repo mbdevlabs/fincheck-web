8e4463644f3f1ee9192f2818d8fc2b0c4b2dca31
marcio ubuntu <mbreno.dev@gmail.com>
2026-02-23 08:19:18 -0300

    fix: load ReactQueryDevtools only in development mode

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ export function App() {
         <Toaster />
       </AuthProvider>
 
-      <ReactQueryDevtools />
+      {import.meta.env.DEV && <ReactQueryDevtools />}
     </QueryClientProvider>
   );
 }
