86dd11067113a167fb0a41648d7b5d002cdc3e3d
marcio ubuntu <mbreno.dev@gmail.com>
2026-02-23 08:19:09 -0300

    refactor: reorganize Dashboard components into modular directory structure
    Move Accounts and Transactions from single files into dedicated
    directories with separated concerns (controller hook, sub-components).

--- src/view/pages/Dashboard/components/Accounts/index.tsx
+++ src/view/pages/Dashboard/components/Accounts/index.tsx
@@ -1,11 +1,13 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { EyeIcon } from "../../../components/icons/EyeIcon";
-import { AccountCard } from "./AccountCard";
+import { EyeIcon } from "../../../../components/icons/EyeIcon";
 
 import "swiper/css";
+import { AccountCard } from "./AccountCard";
 import { AccountSliderNavigation } from "./AccountSliderNavigation";
+import { useAccountsController } from "./useAccountsController";
 
 export function Accounts() {
+  const { setSliderState, sliderState } = useAccountsController();
   return (
     <div className="bg-teal-900 rounded-2xl w-full h-full md:p-10 px-4 py-8 flex flex-col">
       <div>
@@ -26,6 +28,12 @@ export function Accounts() {
           <Swiper
             spaceBetween={16}
             slidesPerView={2}
+            onSlideChange={(swiper) => {
+              setSliderState({
+                isBeginning: swiper.isBeginning,
+                isEnd: swiper.isEnd,
+              });
+            }}
           >
             <div
               className="flex items-center justify-between mb-4"
@@ -35,7 +43,10 @@ export function Accounts() {
                 Minhas contas
               </strong>
 
-              <AccountSliderNavigation />
+              <AccountSliderNavigation
+                isBeginning={sliderState.isBeginning}
+                isEnd={sliderState.isEnd}
+              />
             </div>
 
             <SwiperSlide>
