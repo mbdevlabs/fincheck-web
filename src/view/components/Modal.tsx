bb29043a1c52d99887fd237a1e74c1b64dfc6151
marcio ubuntu <mbreno.dev@gmail.com>
2026-03-10 17:49:09 -0300

    refactor: rename Dialog to RdxDialog and added Rdx.Title

--- src/view/components/Modal.tsx
+++ src/view/components/Modal.tsx
@@ -1,5 +1,5 @@
+import * as RdxDialog from "@radix-ui/react-dialog";
 import { Cross2Icon } from "@radix-ui/react-icons";
-import { Dialog } from "radix-ui";
 import type React from "react";
 import { cn } from "../../app/utils/cn";
 
@@ -19,24 +19,23 @@ export function Modal({
   onClose,
 }: ModalProps) {
   return (
-    <Dialog.Root
+    <RdxDialog.Root
       open={open}
       onOpenChange={onClose}
     >
-      <Dialog.Portal>
-        <Dialog.Overlay
+      <RdxDialog.Portal>
+        <RdxDialog.Overlay
           className={cn(
             "fixed inset-0 bg-black/80 backdrop-blur-sm z-50",
             "data-[state=open]:animate-overlayShow",
           )}
         />
-        <Dialog.Content
+        <RdxDialog.Content
           className={cn(
             "fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 p-6 space-y-10 bg-white rounded-2xl z-51 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)] w-full max-w-100 outline-none ",
             "data-[state=open]:animate-content-show",
           )}
         >
-          <Dialog.Title />
           <header className="h-12 flex items-center justify-between text-gray-800">
             <button
               onClick={onClose}
@@ -45,15 +44,17 @@ export function Modal({
               <Cross2Icon className="w-6 h-6" />
             </button>
 
-            <span className="text-lg tracking=[-1px] font-bold">{title}</span>
+            <RdxDialog.Title>
+              <span className="text-lg tracking=[-1px] font-bold">{title}</span>
+            </RdxDialog.Title>
 
             <div className="w-12 h-12 flex items-center justify-center">
               {rightAction}
             </div>
           </header>
           <div>{children}</div>
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
+        </RdxDialog.Content>
+      </RdxDialog.Portal>
+    </RdxDialog.Root>
   );
 }
